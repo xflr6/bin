e501501124bea0742882f6650a29319c0821e584
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-02-15 09:26:54 +0100

    add conversion of zip-files to squashfs images

---
+++ zip2sfs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+export PATH="/usr/bin:/bin"
+
+WORKDIR="/tmp"
+
+for zip_path in "$@"; do
+    zip_file=${zip_path##*/}
+    zip_name=${zip_file%.*}
+
+    extract="$WORKDIR/${zip_name}.zip2sfs/"
+    result="$zip_name.sfs"
+
+    unzip -n "$zip_path" -d "$extract"
+    mksquashfs "$extract" "$result" -noappend
+
+    chown --reference="$zip_path" "$result"
+    chmod --reference="$zip_path" "$result"
+    touch --reference="$zip_path" "$result"
+
+    rm -rf "$extract"
+
+done
+
+exit 0
