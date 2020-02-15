63ff5ec8324c0d3ba614be1b903878ea780cb647
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-02-15 18:20:22 +0100

    fill-in command-line script skeleton

--- count-wiki.py
+++ count-wiki.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python3
-# wikicount.py - count pages in wikipedia xml export
 
+"""Count page tags in MediaWiki XML export."""
+
+__title__ = 'count-wiki.py'
+__version__ = '0.1.dev0'
+__author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
+__license__ = 'MIT, see LICENSE.txt'
+__copyright__ = 'Copyright (c) 2020 Sebastian Bank'
+
+import argparse
 import bz2
+import functools
+import pathlib
 import re
 import sys
 import time
 import xml.etree.ElementTree as etree
 
-FILENAME = 'dewiki-latest-pages-articles.xml.bz2'
+PAGE_TAG = 'page'
+
+DISPLAY_PATH = 'title'
+
+DISPLAY_AFTER = 1000
 
 MEDIAWIKI_EXPORT = r'http://www\.mediawiki\.org/xml/export-\d+(?:\.\d+)*/'
 
-PAGE_TAG = 'page'
 
-DISPLAY_TAG = 'title'
+def present_file(s):
+    if not s:
+        return None
+    try:
+        result = pathlib.Path(s)
+    except ValueError:
+        result = None
+    if result is None or not result.is_file():
+        raise argparse.ArgumentTypeError(f'not a present file: {s}')
+    return result
 
 
-def ntags(filename, tag=PAGE_TAG, display_tag=DISPLAY_TAG, progress_after=1000):
+def ntags(filename, tag, *, display_path, display_after):
     count = 0
-    start = time.time()
+    start = time.monotonic()
     with bz2.BZ2File(filename) as f:
         pairs = etree.iterparse(f, events=('start', 'end'))
         _, root = next(pairs)
@@ -26,27 +48,47 @@ def ntags(filename, tag=PAGE_TAG, display_tag=DISPLAY_TAG, progress_after=1000):
         assert re.fullmatch(MEDIAWIKI_EXPORT, ns)
         assert root.tag == f'{{{ns}}}mediawiki'
         tag = f'{{{ns}}}{tag}'
-        display_tag = f'{{{ns}}}{display_tag}' if display_tag else None
+        display_path = f'{{{ns}}}{display_path}' if display_path else None
         for event, elem in pairs:
             if elem.tag == tag and event == 'end':
                 count += 1
-                if not (count % progress_after):
+                if not (count % display_after):
                     msg = f'{count:,}'
-                    if display_tag is not None:
-                        msg += f'\t{elem.findtext(display_tag)}'
-                    print(msg)
+                    if display_path is not None:
+                        msg += f'\t{elem.findtext(display_path)}'
+                    log(msg)
                 root.clear()
-    stop = time.time()
+    stop = time.monotonic()
     if verbose:
-        print(f'duration: {stop - start:.2f} seconds')
+        log(f'duration: {stop - start:.2f} seconds')
     return count
 
 
+parser = argparse.ArgumentParser(description=__doc__)
+
+parser.add_argument('filename', type=present_file,
+                    help='path to MediaWiki XML export (.xml.bz2)')
+
+parser.add_argument('--tag', default=PAGE_TAG,
+                    help=f'end tag to count (default: {PAGE_TAG})')
+
+parser.add_argument('--display', metavar='PATH', default=DISPLAY_PATH,
+                    help='ElementPath to log in sub-total'
+                         f' (default: {DISPLAY_PATH})')
+
+parser.add_argument('--display_after', metavar='N', type=int,
+                    default=DISPLAY_AFTER,
+                    help='log sub-total after N tags'
+                         f' (default: {DISPLAY_AFTER})')
+
+parser.add_argument('--version', action='version', version=__version__)
+
+
+log = functools.partial(print, file=sys.stderr, sep='\n')
+
+
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        sys.exit(f'Usage: {sys.argv[0]} <filename>')
-    elif len(sys.argv) == 1:
-        filename = FILENAME
-    elif len(sys.argv) == 2:
-        filename = sys.argv[1]
-    print(ntags(filename))
+    args = parser.parse_args()
+    kwargs = {'display_path': args.display, 'display_after': args.display_after}
+    count = ntags(args.filename, args.tag, **kwargs)
+    print(count)
