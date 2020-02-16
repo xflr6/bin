5899ac33de546870b52d27373b0bd4da56c243b0
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-02-16 18:24:52 +0100

    flatten one level

--- tmux-attach.sh
+++ tmux-attach.sh
@@ -16,11 +16,11 @@ if ! tmux has-session -t "$base_session" 2> /dev/null; then
     fi
 fi
 
-session="${base_session}-$((
-    $(tmux list-sessions -F "#{session_name}" | \
+ord=$(($(tmux list-sessions -F "#{session_name}" | \
     sed -rn "s/^${base_session}-([0-9]+)/\1/p" | \
-    sort -n | tail -1)
-    +  1 ))"
+    sort -n | tail -1) +  1))
+
+session="${base_session}-${ord}"
 
 echo "attach to linked session $session"
 tmux new-session -t "$base_session" -s "$session" \; \
