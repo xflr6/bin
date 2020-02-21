1af543268812758c64b188a760f8768305aba173
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-02-21 17:12:24 +0100

    break into main() and functions

--- blame-wiki.py
+++ blame-wiki.py
@@ -28,6 +28,43 @@ ENCODING = 'utf-8'
 GZIP = 'gzip'
 
 
+log = functools.partial(print, file=sys.stderr, sep='\n')
+
+
+def make_request(url, title, encoding=ENCODING):
+    post = {'pages': title, 'wpDownload': 1}
+    data = urllib.parse.urlencode(post).encode(encoding)
+    kwargs = {'headers': {'accept-encoding': GZIP}}
+    return urllib.request.Request(url, data=data, **kwargs)
+
+
+def parse_response(resp, encoding=ENCODING):
+    info = resp.info()
+    c_headers = 'type', 'disposition', 'encoding'
+    c_values = tuple(info.get(f'content-{h}') for h in c_headers)
+    for h, v in zip(c_headers, c_values):
+        log(f'content-{h}: {v}')
+    ct, cd, ce = c_values
+
+    assert ct == f'application/xml; charset={encoding}'
+    assert cd.startswith('attachment;filename=')
+    assert ce == GZIP
+
+    with gzip.open(resp) as f:
+        return etree.parse(f)
+
+
+def extract_ns(tag):
+    ns = tag.partition('{')[2].partition('}')[0]
+    assert tag.startswith(f'{{{ns}}}')
+    return ns
+
+
+def elem_findtext(elem, *tags, **kwargs):
+    values = (elem.findtext(f'ns:{t}', **kwargs) for t in tags)
+    return dict(zip(tags, values))
+
+
 parser = argparse.ArgumentParser(description=__doc__)
 
 parser.add_argument('page_title', help='title of the page on MediaWiki')
@@ -39,59 +76,43 @@ parser.add_argument('--export-url', metavar='URL', default=EXPORT_URL,
 
 parser.add_argument('--version', action='version', version=__version__)
 
-args = parser.parse_args()
 
-log = functools.partial(print, file=sys.stderr, sep='\n')
+def main(args=None):
+    args = parser.parse_args(args)
+    log(f'export url: {args.export_url}', f'title: {args.page_title}', '')
 
-post = urllib.parse.urlencode({'pages': args.page_title, 'wpDownload': 1})
-req = urllib.request.Request(args.export_url,
-                             data=post.encode(ENCODING),
-                             headers={'accept-encoding': GZIP})
+    req = make_request(args.export_url, args.page_title)
+    log(f'urllib.request.urlopen({req})')
+    with urllib.request.urlopen(req) as f:
+        tree = parse_response(f)
 
-log(f'export url: {args.export_url}', f'post: {post}', '')
+    root_tag = tree.getroot().tag
+    ns = extract_ns(root_tag)
+    log(f'xml: {ns}')
+    assert re.fullmatch(MEDIAWIKI_EXPORT, root_tag)
+    ns = {'namespaces': {'ns': ns}}
 
-log(f'urllib.request.urlopen({req})')
-with urllib.request.urlopen(req) as f:
-    c_headers = 'type', 'disposition', 'encoding'
-    c_values = tuple(f.info().get(f'content-{h}') for h in c_headers)
-    for h, v in zip(c_headers, c_values):
-        log(f'content-{h}: {v}')
-    ct, cd, ce = c_values
+    info, = tree.findall('ns:siteinfo', **ns)
+    for k, v in elem_findtext(info, 'sitename', 'dbname', 'base', **ns).items():
+        log(f'siteinfo/{k}: {v}')
 
-    assert ct == f'application/xml; charset={ENCODING}'
-    assert cd.startswith('attachment;filename=')
-    assert ce == GZIP
+    page, = tree.findall('ns:page', **ns)
+    infos = elem_findtext(page, 'ns', 'title', 'id', **ns)
+    for k, v in infos.items():
+        log(f'page/{k}: {v}')
 
-    with gzip.open(f) as z:
-        tree = etree.parse(z)
+    assert infos['ns'] == '0'
+    assert infos['title'] == args.page_title
 
-root_tag = tree.getroot().tag
-ns = root_tag.partition('{')[2].partition('}')[0]
-log(f'xml: {ns}')
-assert root_tag.startswith(f'{{{ns}}}')
-assert re.fullmatch(MEDIAWIKI_EXPORT, root_tag)
-ns = {'namespaces': {'ns': ns}}
+    log(f'search string: {args.search_string}')
+    for r in page.iterfind('ns:revision', **ns):
+        if args.search_string in r.findtext('ns:text', '', **ns):
+            log()
+            etree.dump(r)
+            return None
 
-info, = tree.findall('ns:siteinfo', **ns)
-i_keys = 'sitename', 'dbname', 'base'
-i_values = tuple(info.findtext(f'ns:{k}', **ns) for k in i_keys)
-for k, v in zip(i_keys, i_values):
-    log(f'siteinfo/{k}: {v}')
+    return 'not found'
 
-page, = tree.findall('ns:page', **ns)
-p_keys = 'ns', 'title', 'id'
-p_values = tuple(page.findtext(f'ns:{k}', **ns) for k in p_keys)
-for k, v in zip(p_keys, p_values):
-    log(f'page/{k}: {v}')
 
-p_ns, p_title, _ = p_values
-assert p_ns == '0'
-assert p_title == args.page_title
-
-log(f'search string: {args.search_string}')
-for r in page.iterfind('ns:revision', **ns):
-    if args.search_string in r.findtext('ns:text', '', **ns):
-        log()
-        etree.dump(r)
-
-        break
+if __name__ == '__main__':
+    sys.exit(main())
