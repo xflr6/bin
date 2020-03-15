6ce657fece7edd66a961b2732d04ff827723c927
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-03-15 13:20:47 +0100

    reorganize test fixtures

--- tests/test_log-pings.py
+++ tests/test_log-pings.py
@@ -7,59 +7,59 @@ import pytest
 
 log_pings = importlib.import_module('log-pings')
 
-IP_HEADER = {'version_ihl': 0x45, 'tos': 0,
-             'length': 60,
-             'ident': 15,
-             'flags_fragoffset': 0,
-             'ttl': 42, 'proto': 1,
-             'hdr_checksum': 0x92af,
-             'src_addr': '127.0.0.2',
-             'dst_addr': '127.0.0.1'}
-
-ICMP_HEADER = {'type': 8, 'code': 0,
-               'checksum': 0x4c33,
-               'ident': 255,
-               'seq_num': 42}
-
 MSG = 'abcdefghijklmnopqrstuvwabcdefghi'
 
 
 @pytest.fixture
-def packet(encoding='utf-8'):
-    ip = log_pings.IPHeader(**IP_HEADER)
+def ip_header():
+    ip = log_pings.IPHeader(version_ihl=0x45, tos=0,
+                            length=60,
+                            ident=15,
+                            flags_fragoffset=0,
+                            ttl=42, proto=1,
+                            hdr_checksum=0x92af,
+                            src_addr='127.0.0.2',
+                            dst_addr='127.0.0.1')
 
+    assert ip.to_bytes() == (b'\x45\x00'
+                             b'\x00\x3c'
+                             b'\x00\x0f'
+                             b'\x00\x00'
+                             b'\x2a\x01'
+                             b'\x92\xaf'
+                             b'\x7f\x00\x00\x02'
+                             b'\x7f\x00\x00\x01')
+
+    return ip
+
+
+@pytest.fixture
+def icmp_packet(encoding='utf-8'):
     msg = MSG.encode(encoding)
-    icmp = log_pings.ICMPPacket(payload=msg, **ICMP_HEADER)
 
-    raw = ip.to_bytes() + icmp.to_bytes()
+    icmp = log_pings.ICMPPacket(type=8, code=0,
+                                checksum=0x4c33,
+                                ident=255,
+                                seq_num=42,
+                                payload=msg)
 
-    assert raw == (# IP_HEADER
-                   b'\x45\x00'  # version=4 ihl=5 tos=0
-                   b'\x00\x3c'  # length=60
-                   b'\x00\x0f'  # ident=15
-                   b'\x00\x00'  # flags=0 fragoffset=0
-                   b'\x2a\x01'  # ttl=42 proto=1
-                   b'\x92\xaf'  # hdr_checksum=37551
-                   b'\x7f\x00\x00\x02'  # scr_addr='127.0.0.2'
-                   b'\x7f\x00\x00\x01'  # dst_addr='127.0.0.1'
-                   # ICMP_HEADER
-                   b'\x08\x00'  # type=8 code=0
-                   b'\x4c\x33'  # checksum=19507
-                   b'\x00\xff'  # ident=255
-                   b'\x00\x2a'  # seq_num=42
-                   + msg)
+    assert icmp.to_bytes() == (b'\x08\x00'
+                               b'\x4c\x33'
+                               b'\x00\xff'
+                               b'\x00\x2a'
+                               + msg)
 
-    return argparse.Namespace(ip=ip, icmp=icmp)
+    return icmp
 
 
 @pytest.mark.usefixtures('mock_pwd_grp')
-def test_log_pings(capsys, mocker, packet, host='127.0.0.1'):
+def test_log_pings(capsys, mocker, ip_header, icmp_packet, host='127.0.0.1'):
     socket = mocker.patch('socket.socket', autospec=True)
 
     packets = iter([
-        packet.ip.to_bytes() + packet.icmp.to_bytes(),
-        packet.ip.to_bytes() + packet.icmp._replace(payload=b'abcde', checksum=0xcd0f).to_bytes(),
-        packet.ip._replace(hdr_checksum=0).to_bytes() + packet.icmp.to_bytes(),
+        ip_header.to_bytes() + icmp_packet.to_bytes(),
+        ip_header.to_bytes() + icmp_packet._replace(payload=b'abcde', checksum=0xcd0f).to_bytes(),
+        ip_header._replace(hdr_checksum=0).to_bytes() + icmp_packet.to_bytes(),
     ])
 
     def recv_into(buf):
