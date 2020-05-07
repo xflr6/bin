0d40d39122e76655abbf0a8b03bc1dc213099e91
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-05-07 20:53:52 +0200

    remove explicit backlog

--- serve-asciimation.py
+++ serve-asciimation.py
@@ -251,7 +251,6 @@ def main(args=None):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((args.host, args.port))
-    s.listen(5)
     logging.debug('%r', s)
 
     if args.hardening:
