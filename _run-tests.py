cc4535add68da5e1d8ae948ed8f8d91de4da71c0
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-12-04 11:40:45 +0100

    increase verbosity

--- _run-tests.py
+++ _run-tests.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
+"""Run the tests with https://pytest.org."""
+
+import pathlib
 import platform
 import sys
 
 import pytest
+
+SELF = pathlib.Path(__file__)
 
 ARGS = [#'--collect-only',
         #'--verbose',
         #'--pdb',
         #'--exitfirst',  # a.k.a. '-x'
         #'-W', 'error',
-        ]
+       ]
 
 if platform.system() == 'Windows':
     ARGS.append('--pdb')
 
-sys.exit(pytest.main(ARGS + sys.argv[1:]))
+
+print('run', [SELF.name] + sys.argv[1:])
+args = ARGS + sys.argv[1:]
+
+print(f'pytest.main({args!r})')
+sys.exit(pytest.main(args))
