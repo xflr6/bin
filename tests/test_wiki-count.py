643948bc2bd1598af12be7e9cd7a3eaf9baa5733
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2026-09-15 09:56:51 +0200

    add test coverage for wiki-count.py --stats

--- tests/test_wiki-count.py
+++ tests/test_wiki-count.py
@@ -1,5 +1,7 @@
 import bz2
+import gzip
 import importlib
+import textwrap
 
 wiki_count = importlib.import_module('wiki-count')
 
@@ -11,9 +13,29 @@ EXPORT = '''
   </page>
   <page>
     <title>Spam</title>
+    <revision>
+      <contributor><username>Vikings</username></contributor>
+      <text>Spam, Spam, Spam, Spam</text>
+    </revision>
+    <revision>
+      <contributor><username>Vikings</username></contributor>
+      <text>Spam, Spam, Spam, Spam</text>
+    </revision>
+    <revision>
+      <contributor><username>Vikings</username></contributor>
+      <text>Spam, Spam, Spam, Spam</text>
+    </revision>
+    <revision>
+      <contributor><username>Vikings</username></contributor>
+      <text>Lovely Spam! Wonderful Spam!</text>
+    </revision>
   </page>
   <page>
     <title>Eggs</title>
+    <revision>
+      <contributor><username>Waitress</username></contributor>
+      <text>Eggs and bacon</text>
+    </revision>
   </page>
 </mediawiki>
 '''.lstrip()
@@ -36,4 +58,27 @@ def test_main(capsys, tmp_path):
 
     (out, err) = capsys.readouterr()
     assert out == '2\n'
+    assert not err
+
+
+def test_main_gz_stats(capsys, tmp_path):
+    export = tmp_path / 'spamwiki-latest-pages-articles.xml.gz'
+
+    with (export.open(mode='wb') as z,
+          gzip.open(z, mode='wt', encoding=ENCODING) as f):
+        f.write(EXPORT)
+
+    assert wiki_count.main([str(export), '--stats']) is None
+
+    (out, err) = capsys.readouterr()
+    assert out == textwrap.dedent(
+        '''
+        2
+
+        Vikings         \t4
+        Waitress        \t1
+
+        Vikings         \t3
+        Waitress        \t2
+        ''').lstrip()
     assert not err
